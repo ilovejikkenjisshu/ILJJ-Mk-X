4acfca084b8f1167a858be40b09c1f7847e1f22a
proelbtn <proelbtn@users.noreply.github.com>
2018-12-31 15:08:05 +0900

    add simple sample code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+#[macro_use]
+extern crate serenity;
+
+use serenity::client::Client;
+use serenity::framework::standard::StandardFramework;
+use serenity::prelude::EventHandler;
+use std::env;
+
+struct Handler;
+
+impl EventHandler for Handler {}
+
 fn main() {
-    println!("Hello, world!");
+    let mut client = Client::new(&env::var("DISCORD_TOKEN").expect("token"), Handler)
+        .expect("Error creating client");
+    client.with_framework(
+        StandardFramework::new()
+            .configure(|c| c.prefix("!mkx "))
+            .cmd("ping", ping),
+    );
+
+    if let Err(why) = client.start() {
+        println!("{:?}", why);
+    }
 }
+
+command!(ping(_context, message) {
+    let _ = message.reply("Pong!");
+});
