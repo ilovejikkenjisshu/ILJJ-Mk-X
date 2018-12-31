9e92f807f44c59b8e7364376074e9fbe57dc3fa1
proelbtn <proelbtn@users.noreply.github.com>
2018-12-31 17:26:15 +0900

    add code

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,67 @@
-#[macro_use]
-extern crate serenity;
-
 use serenity::client::Client;
-use serenity::framework::standard::StandardFramework;
-use serenity::prelude::EventHandler;
+use serenity::model::channel::Message;
+use serenity::prelude::{Context, EventHandler};
+use std::collections::HashMap;
 use std::env;
+use std::fmt;
+use std::sync::Mutex;
 
-struct Handler;
+struct ILJJ_Mk_X {
+    entries: HashMap<&'static str, Box<Mutex<Command + Send>>>,
+}
+impl ILJJ_Mk_X {
+    fn new(entries: HashMap<&'static str, Box<Mutex<Command + Send>>>) -> ILJJ_Mk_X {
+        ILJJ_Mk_X { entries }
+    }
+}
 
-impl EventHandler for Handler {}
+trait Command {
+    fn help(&self) -> &str {
+        return "no description";
+    }
+
+    fn execute(&self, ctx: &Context, msg: &Message) {
+        msg.reply("not implemented");
+    }
+}
+
+struct DebugPrint;
+impl Command for DebugPrint {
+    fn execute(&self, ctx: &Context, msg: &Message) {
+        let reply: String = format!("{:?}", msg);
+        msg.reply(reply.as_str());
+    }
+}
+
+impl EventHandler for ILJJ_Mk_X {
+    fn message(&self, ctx: Context, msg: Message) {
+        if msg.content.starts_with("!mkx") {
+            let argv: Vec<&str> = msg.content.split(" ").collect();
+
+            let cmd = argv.get(1).unwrap();
+
+            match self.entries.get(cmd) {
+                Some(module) => {
+                    let module = module.lock().unwrap();
+                    module.execute(&ctx, &msg);
+                }
+                None => {
+                    msg.reply("no such command");
+                }
+            }
+        }
+    }
+}
 
 fn main() {
-    let mut client = Client::new(&env::var("DISCORD_TOKEN").expect("token"), Handler)
-        .expect("Error creating client");
-    client.with_framework(
-        StandardFramework::new()
-            .configure(|c| c.prefix("!mkx "))
-            .cmd("ping", ping),
-    );
+    let mut entries: HashMap<&str, Box<Mutex<Command + Send>>> = HashMap::new();
+    entries.insert("debug", Box::new(Mutex::new(DebugPrint)));
+
+    let mkx = ILJJ_Mk_X::new(entries);
+    let token = &env::var("DISCORD_TOKEN").unwrap();
+    let mut client = Client::new(token, mkx).unwrap();
 
     if let Err(why) = client.start() {
         println!("{:?}", why);
     }
 }
-
-command!(ping(_context, message) {
-    let _ = message.reply("Pong!");
-});
